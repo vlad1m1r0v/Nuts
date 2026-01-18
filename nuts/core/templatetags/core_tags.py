ae0d5a3f1b0f23feca3f25b7e093aedef0adf4f8
ant100pshnik <mypochtav@gmail.com>
2026-01-18 17:50:11 +0200

    split base template into 'includes' parts. Add tag for breadcrumbs

--- nuts/core/templatetags/core_tags.py
+++ nuts/core/templatetags/core_tags.py
@@ -8,7 +8,6 @@ from payment_and_delivery.models import PaymentAndDeliveryPage
 from customers.models import CustomersPage
 from news.models import NewsIndexPage
 
-
 register = template.Library()
 
 
@@ -43,6 +42,7 @@ def format_phone_html(value):
 
     return value
 
+
 @register.inclusion_tag('tags/menu_links.html', takes_context=True)
 def menu_links(context):
     return {
@@ -52,4 +52,33 @@ def menu_links(context):
         'customers_page': CustomersPage.objects.live().first(),
         'news_page': NewsIndexPage.objects.live().first(),
         'request': context.get('request')
+    }
+
+
+@register.inclusion_tag('tags/language_switcher.html', takes_context=True)
+def language_switcher(context):
+    request = context.get('request')
+    page = context.get('page') or context.get('self')
+
+    query_parameters = request.GET.urlencode()
+
+    return {
+        'request': request,
+        'page': page,
+        'query_parameters': query_parameters
+    }
+
+
+@register.inclusion_tag('tags/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context):
+    self = context.get('self') or context.get('page')
+
+    if self is None:
+        return {'ancestors': None}
+
+    ancestors = self.get_ancestors(inclusive=True).filter(depth__gte=2)
+
+    return {
+        'ancestors': ancestors,
+        'request': context.get('request'),
     }
